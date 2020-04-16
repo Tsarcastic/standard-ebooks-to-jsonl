bc8f1d89268390b6301888a7212148f7b5e6c557
Brendan Davis <BrendanMD@gmail.com>
2020-04-16 16:58:34 -0700

    removed first 29 characters from every tag entry

--- xml_to_jsonl.py
+++ xml_to_jsonl.py
@@ -13,11 +13,14 @@ def parse_xml(xmlfile):
 
 def write_dict(parsed_file):
     """To transfer the information to a dictionary."""
-    # let's write a dictionary
+    # Iterate through the parsed file
     for entry in parsed_file:
-        # for next_level in entry:
-        if entry.tag == '{http://www.w3.org/2005/Atom}entry': 
-            print(entry[1].text)
+        if entry.tag == '{http://www.w3.org/2005/Atom}entry':
+            # Position set at 29 to correctly grab title
+            # Currently will print "title" followed by title
+            current_count = 1
+            current_entry = entry[current_count]
+            print((current_entry.tag)[29:], current_entry.text)
 
 
 write_dict(parse_xml('ebooks.xml'))
