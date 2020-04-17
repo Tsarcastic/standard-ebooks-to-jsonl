ef0ea224ff5ce82ba59e2c0f7bc3ec7bc3ff51dd
Brendan Davis <BrendanMD@gmail.com>
2020-04-17 12:08:47 -0700

    returning a list of books as dictionaries. need to write conditionals for grandchildren & elements with info stored outside of text

--- xml_to_jsonl.py
+++ xml_to_jsonl.py
@@ -4,6 +4,8 @@ import xml.etree.ElementTree as ET
 import jsonlines
 
 
+
+
 def parse_xml(xmlfile):
     """To parse the XML file."""
     # create element tree object
@@ -14,11 +16,18 @@ def parse_xml(xmlfile):
 def write_dict(parsed_file):
     """To transfer the information to a dictionary."""
     # Iterate through the parsed file
+    ebooks = []
     for entry in parsed_file:
         if entry.tag == 'entry':
-            # Currently will print "title" followed by title
-            current_count = 1
-            print(entry[current_count].tag, entry.[current_count].text
+            book = {}
+            for entry_child in entry:
+                if entry_child.tag in book:
+                    book[entry_child.tag].append(entry_child.text)
+                else:
+                    book[entry_child.tag] = [entry_child.text]
+            ebooks.append(book)
+    return ebooks
 
+print(write_dict(parse_xml('alt_ebooks.xml')))
 
 write_dict(parse_xml('alt_ebooks.xml'))
