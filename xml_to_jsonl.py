b55c42791ee15f240b365eb00f4062ba36038a62
Brendan Davis <BrendanMD@gmail.com>
2020-04-17 11:03:43 -0700

    removed namespaces from xml and cleaned python code

--- xml_to_jsonl.py
+++ xml_to_jsonl.py
@@ -15,12 +15,10 @@ def write_dict(parsed_file):
     """To transfer the information to a dictionary."""
     # Iterate through the parsed file
     for entry in parsed_file:
-        if entry.tag == '{http://www.w3.org/2005/Atom}entry':
-            # Position set at 29 to correctly grab title
+        if entry.tag == 'entry':
             # Currently will print "title" followed by title
             current_count = 1
-            current_entry = entry[current_count]
-            print((current_entry.tag)[29:], current_entry.text)
+            print(entry[current_count].tag, entry.[current_count].text
 
 
-write_dict(parse_xml('ebooks.xml'))
+write_dict(parse_xml('alt_ebooks.xml'))
